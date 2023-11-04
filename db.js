78a74b7dc694917a4d6a04e6ea7285da7f2735ea
Akashsnar <107642548+Akashsnar@users.noreply.github.com>
2023-11-04 09:50:49 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,17 +1,25 @@
 const mongoose = require('mongoose');
 
-const mongoURI = "mongodb://0.0.0.0:27017/inotebook"
+// const mongoURI = "mongodb://0.0.0.0:27017/inotebook"
 
 const connectToMongo = ()=>{
-    console.log("chal na");
-    try {
-    mongoose.connect(mongoURI, ()=>{
-        console.log("Connected to Mongo Successfully");
-    })
-}
-catch (error) {
-    console.log(error);
-}
+    // console.log("chal na");
+    // try {
+    // mongoose.connect(mongoURI, ()=>{
+    //     console.log("Connected to Mongo Successfully");
+    // })
+
+
+    mongoose.set('strictQuery', false);
+        try {
+            const uri = "mongodb+srv://aksn0204:K1M2XyNiTC54KDFB@cluster0.2knilcp.mongodb.net/notesdata";
+            await mongoose.connect(uri);
+            console.log("connected to mongodb");
+        }
+        catch (error) {
+            console.log(error);
+        }
+
 }
 
 module.exports = connectToMongo;
